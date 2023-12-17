9d80b1a48c44376135ff79df89e50254ea8e238d
KorolevAndrey <44860813+KorolevAndrey@users.noreply.github.com>
2023-12-17 21:36:16 +0300

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,6 +1,22 @@
 
+
 // [[Page Appearance
-//xui.ini.$PageAppearance = {
-//    "theme":"webflat"
-//};
+xui.ini.$PageAppearance = {
+    "theme" : "moonify"
+};
 // ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf" : {
+        "width" : 1280,
+        "height" : 1024
+    }
+};
+// ]]Develop Env Setting
